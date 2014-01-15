2895d3c66e5bada08e8306e107f809ed576e95ce
John Okahata <jokahata@github.com>
2014-01-14 20:54:21 -0800

    Add in correct css highlighting when returning to page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,47 +7,51 @@ class MoviesController < ApplicationController
   end
 
   def index
-    
+
     @movies = Movie.all
-	@all_ratings = Movie.get_ratings
+    @all_ratings = Movie.get_ratings
+    @is_sort_title = false
+    @is_sort_release = false
 
-	#If there's a new ordering use it
-	if (params[:order] != nil)
-		session[:order] = params[:order]
-	end
-	
-	#Declares and initializes a Hash that holds whether a box is checked or not
-	if (session[:ratings] == nil)
-		session[:ratings] = Hash.new
-		@all_ratings.each do |rating|
-			session[:ratings][rating] = true
-		end
-	end
+  #If there's a new ordering use it
+  if (params[:order] != nil)
+    session[:order] = params[:order]
+  end
+
+  #Declares and initializes a Hash that holds whether a box is checked or not
+  if (session[:ratings] == nil)
+    session[:ratings] = Hash.new
+    @all_ratings.each do |rating|
+      session[:ratings][rating] = true
+    end
+  end
 
 
-	#If one or more ratings are selected
-	if(params[:ratings] != nil)
-		#Goes through the checkbox hash and set false for unchecked boxes
-		session[:ratings].each_key do |rating|
-			params[:ratings].has_key?(rating) ? (session[:ratings][rating] = true) : (session[:ratings][rating] = false)
-		end
-	else
-		flash.keep
-		#redirect_to movies_path, :ratings => session[:ratings]
-	end
+  #If one or more ratings are selected
+  if(params[:ratings] != nil)
+    #Goes through the checkbox hash and set false for unchecked boxes
+    session[:ratings].each_key do |rating|
+      params[:ratings].has_key?(rating) ? (session[:ratings][rating] = true) : (session[:ratings][rating] = false)
+    end
+  else
+    flash.keep
+    #redirect_to movies_path, :ratings => session[:ratings]
+  end
 
-	#@movies is an array so I use select
-	@movies = @movies.select { |m| session[:ratings][m.rating]}
-	
-	#Sorting procedures
-	if (session[:order] != nil)
-		if (session[:order] == 'title')
-			@movies.sort_by!(&:title) #same as @movies.sort_by{|m| m.title}
-		end
-		if (session[:order] == 'release')
-			@movies.sort_by!(&:release_date)
-		end
-	end
+  #@movies is an array so I use select
+  @movies = @movies.select { |m| session[:ratings][m.rating]}
+
+  #Sorting procedures
+  if (session[:order] != nil)
+    if (session[:order] == 'title')
+      @is_sort_title = true
+      @movies.sort_by!(&:title) #same as @movies.sort_by{|m| m.title}
+    end
+    if (session[:order] == 'release')
+      @is_sort_release = true
+      @movies.sort_by!(&:release_date)
+    end
+  end
   end
 
   def new
